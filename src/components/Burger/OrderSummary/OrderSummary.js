4fa1bd1c14a779312e39b416120dfd47a9226e17
tolmamal <tolmamal@gmail.com>
2020-02-11 19:12:51 +0200

    order summary

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Auxx from '../../../hoc/Auxx';
+
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+            <li key={igKey}>
+                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+            </li>
+            )
+        });
+    return (
+        <Auxx>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+
+        </Auxx>
+    )
+
+};
+
+export default orderSummary;
