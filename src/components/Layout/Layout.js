049e324991b502dba62ddfe0e5ce52ddaf6b401f
tolmamal <tolmamal@gmail.com>
2020-02-17 16:42:40 +0200

    DrawerToggle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Auxx from '../../hoc/Auxx';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxx>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxx>
-);
+class Layout extends Component {
+    state = {
+      showSideDrawer: true
+    };
 
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    };
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    };
+
+    render() {
+        return (
+            <Auxx>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxx>
+
+        )
+    }
+
+}
+
+
+export default Layout;
